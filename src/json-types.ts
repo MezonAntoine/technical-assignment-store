c96749083e037388476a5db60ba18d67b37eb6a5
AntoineMezon <antoine.mezon@epitech.eu>
2024-02-29 11:37:17 +0100

    feat(architecture): update architecture project

--- src/json-types.ts
+++
@@ -1,9 +0,0 @@
-export type JSONPrimitive = string | number | boolean | null;
-
-export type JSONValue = JSONPrimitive | JSONArray | JSONObject;
-
-export interface JSONObject {
-  [key: string]: JSONValue;
-}
-
-export type JSONArray = JSONValue[];
